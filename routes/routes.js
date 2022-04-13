372eb4b06439be91baaad9dc256596d5a671c8f7
gaddi sunil <sunilgaddi917@gmail.com>
2022-04-13 11:45:25 +0530

    removed api

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,15 @@
 const router = require('express').Router()
 const axios = require('axios')
 
-router.get('/company', (req, res)=> {
-    axios.get('https://72ih8opnm2.execute-api.ap-south-1.amazonaws.com/live').then( (response)=>{
+// router.get('/company', (req, res)=> {
+//     axios.get('https://72ih8opnm2.execute-api.ap-south-1.amazonaws.com/live').then( (response)=>{
     
-        const result = response.data
+//         const result = response.data
 
-      
-        res.status(200).json({msg:result}) 
-    })
-})
+//       console.log(response.data)
+//         res.status(200).json({msg:result}) 
+
+//     })
+// })
 
 module.exports = router
--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-app.use('/search', require('./routes/routes'))
+
+
+// app.use('/search', require('./routes/routes'))
 
 
 app.listen(8080, ()=> {
